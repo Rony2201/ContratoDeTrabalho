c902ce2ec7725d0881eb233f9df070d9397f21d1
Rony pinheiro <roniely_pinheiro@hotmail.com>
2022-11-04 20:19:12 -0300

    first commit

---
+++ Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace estudocsharp.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
---
+++ Entities/HourContract.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace estudocsharp.Entities
+{
+    class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract()
+        {
+
+        }
+
+        public HourContract(DateTime date, double valuePerHour, int hours)
+        {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+
+        public double TotalValue ()
+        {
+            return ValuePerHour * Hours;
+        }
+
+    }
+}
